b2833d519c527ea4f18c6b6fbee30c78635b3f5a
aleksej <Onyx_2009@li.ru>
2017-10-06 22:06:14 +0400

    Initial Commit

---
+++ Obj21/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Obj21
+//
+//  Created by Алексей on 06.10.17.
+//  Copyright © 2017 Алексей. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Obj21/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Obj21
+//
+//  Created by Алексей on 06.10.17.
+//  Copyright © 2017 Алексей. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
